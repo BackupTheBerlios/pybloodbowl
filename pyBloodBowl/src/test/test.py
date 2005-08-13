3e23e33a507827cdf5b9dd696646dd9ecfc56676
xturbo77 <xturbo77>
2005-08-13 20:34:57 +0000

    unittest modules

--- pyBloodBowl/src/test/test.py
+++ pyBloodBowl/src/test/test.py
@@ -10,7 +10,7 @@ import pyBBPlayer
 
 class BBTest(unittest.TestCase):
     """
-    A test class for the pyBBSkill module.
+    A test class for the pyBloodBowl project.
     """
     
     def setUp(self):
@@ -31,29 +31,33 @@ class BBTest(unittest.TestCase):
     def testSkills(self):
         self.skills = self.skillparser.getSkills()
         self.failUnless(type(self.skills) == type({}), "Skills must be a dictionary")
-#        for skill in self.skills.keys():
-#            s = pyBBSkill.BBSkill(skill)
-#            print s.__repr__().encode("latin-1")
-#            pprint(s.getSkillname())
-#            pprint(s.isTrait())
-#            pprint(s.getCategory())
-#            pprint(s.getDescription())
+        for skill in self.skills.keys():
+            skillobj = pyBBSkill.BBSkill(skill, self.skillparser)
+            self.failUnless(isinstance(skillobj, pyBBSkill.BBSkill), "Skills must be an instance of pyBBSkill.BBSkill")
+        #print "testSkills................................................passed"
+        
+    def testSkill(self):
+        skill = pyBBSkill.BBSkill("Blocken", self.skillparser)
+        self.failUnless(skill.getSkillname() == "Blocken", "Wrong skillname")
+        self.failUnless(skill.getCategory() == "Allgeimein", "Wrong category")
+        self.failUnless(skill.isTrait() == False, "Skill should not be trait")
+        #print "testSkill.................................................passed"
             
     def testTeamProperties(self):
         self.team = pyBBTeam.BBTeam("Rippers","Chaos", "Borak Killer", 4, 3)
         self.failUnless(self.team.getTeamProperties() == ('Rippers', 'Chaos', 'Borak Killer', 3, 0, 0), "Wrong team properties")
-        print "testTeamProperties........................................passed"
+        #print "testTeamProperties........................................passed"
         
     def testTeamSquad(self):
         self.team = pyBBTeam.BBTeam("Rippers","Chaos", "Borak Killer", 4, 3)
         self.failUnless(isinstance(self.team.getSquad(), pyBBSquad.BBSquad), "Squad must be instance of pyBBSquad.BBSquad")                    
-        print "testTeamSquad.............................................passed"
+        #print "testTeamSquad.............................................passed"
         
     def testSquadSize(self):
-        self.squad = pyBBSquad.BBSquad("Amazonen")
-        p1 = pyBBPlayer.BBPlayer("Heino","Amazonen","Blitzer")
-        p2 = pyBBPlayer.BBPlayer("Ludwig","Amazonen","Werfer")
-        p3 = pyBBPlayer.BBPlayer("Rudi","Amazonen","Werfer")
+        self.squad = pyBBSquad.BBSquad(self.teamparser, "Amazonen")
+        p1 = pyBBPlayer.BBPlayer("Heino","Amazonen","Blitzer", self.teamparser, self.skillparser)
+        p2 = pyBBPlayer.BBPlayer("Ludwig","Amazonen","Werfer", self.teamparser, self.skillparser)
+        p3 = pyBBPlayer.BBPlayer("Rudi","Amazonen","Werfer", self.teamparser, self.skillparser)
         self.squad.addPlayer(1,p1)
         self.squad[2] = p2
         self.failUnless(len(self.squad) == 2, "Wrong number of squad members")
@@ -63,17 +67,33 @@ class BBTest(unittest.TestCase):
         self.failUnless(len(self.squad) == 1, "Wrong number of squad members")
         self.squad.removePlayer(1)
         self.failUnless(len(self.squad) == 0, "Wrong number of squad members")
-        print "testSquadSize.............................................passed"
+        #print "testSquadSize.............................................passed"
     
     def testSquadValidation(self):
-        self.squad = pyBBSquad.BBSquad("Amazonen")
-        p1 = pyBBPlayer.BBPlayer("Heino","Chaos","Tiermensch")
-        p2 = pyBBPlayer.BBPlayer("Ludwig","Chaoszwerge","Hobgoblin")
-        p3 = pyBBPlayer.BBPlayer("Rudi","Amazonen","Werfer")
+        self.squad = pyBBSquad.BBSquad(self.teamparser, "Amazonen")
+        p1 = pyBBPlayer.BBPlayer("Heino","Chaos","Tiermensch", self.teamparser, self.skillparser)
+        p2 = pyBBPlayer.BBPlayer("Ludwig","Chaoszwerge","Hobgoblin", self.teamparser, self.skillparser)
+        p3 = pyBBPlayer.BBPlayer("Rudi","Amazonen","Werfer", self.teamparser, self.skillparser)
         self.assertEqual(self.squad.isValidPlayer(p1), False, "Player should not be valid")
         self.assertEqual(self.squad.isValidPlayer(p2), False, "Player should not be valid")
         self.assertEqual(self.squad.isValidPlayer(p3), True, "Player should be valid")
-        print "testSquadValidation.......................................passed"
+        #print "testSquadValidation.......................................passed"
+        
+    def testSquadGetPlayers(self):
+        self.squad = pyBBSquad.BBSquad(self.teamparser, "Amazonen")
+        p1 = pyBBPlayer.BBPlayer("Lara","Amazonen","Feldspieler", self.teamparser, self.skillparser)
+        p2 = pyBBPlayer.BBPlayer("Tara","Amazonen","Feldspieler", self.teamparser, self.skillparser)
+        p3 = pyBBPlayer.BBPlayer("Mara","Amazonen","Werfer", self.teamparser, self.skillparser)
+        self.squad.addPlayer(1, p1)
+        self.squad.addPlayer(3, p2)
+        self.squad.addPlayer(5, p3)
+        playersdict = self.squad.getPlayers()
+        self.failUnless(type(playersdict) == type({}), "Method getPlayers() should return a dictionary")
+        self.failUnless(playersdict.has_key(1), "Player dictionary should contain no 1")
+        self.failUnless(playersdict.has_key(3), "Player dictionary should contain no 3")
+        self.failUnless(playersdict.has_key(5), "Player dictionary should contain no 5")
+        self.failIf(playersdict.has_key(2), "Player dictionary should not contain no 2")
+        #print "testSquadGetPlayers.......................................passed"
         
 if __name__ == '__main__':
     unittest.main()
